a33fa23993e35bc0dfd5c4af82e7e6c464c414ae
Shagufta Khan <shagufta.khan@intelliswift.co.in>
2019-10-01 00:30:34 +0530

    Formatted

--- src/main/java/com/example/graphQLResolver/resolver/Query.java
+++ src/main/java/com/example/graphQLResolver/resolver/Query.java
@@ -10,18 +10,14 @@ import com.example.graphQLResolver.domain.Activity;
 import com.example.graphQLResolver.repository.ActivityRepository;
 
 @Component
-public class Query implements GraphQLQueryResolver{
-    @Autowired
+public class Query implements GraphQLQueryResolver {
+	@Autowired
 	ActivityRepository activityRepository;
 
-
-
 	Activity findActivity(String id) {
-		
 		return find(id);
-
 	}
-	
+
 	Activity find(String id) {
 		Optional<Activity> activityOptional = activityRepository.findById(id);
 		if (!activityOptional.isPresent() || activityOptional.toString().isEmpty()) {
